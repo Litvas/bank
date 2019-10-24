d741d137a52d1ed6f4cc2206b17946fef9ced729
AS <litvas13@gmail.com>
2019-10-24 19:35:20 +0300

    1. add liquidbase changesets for fill test data 2. add relation beetween domains

--- src/main/java/com/litvas/bank/domain/Worker.java
+++ src/main/java/com/litvas/bank/domain/Worker.java
@@ -4,6 +4,7 @@ import lombok.Data;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 @Data
@@ -17,6 +18,7 @@ public class Worker {
     private String lastname;
     private Long salary;
     private String position;
+    @OneToOne
     private Project currentProject;
 
 }
